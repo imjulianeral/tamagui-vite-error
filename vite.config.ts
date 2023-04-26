b8bcaaedc281c7a0688d367eca9c266f57bbd2b1
Julián Acero <julianacero98@gmail.com>
2023-04-26 12:52:51 -0600

    install the vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import { tamaguiPlugin } from '@tamagui/vite-plugin'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tamaguiPlugin({
+      config: './tamagui.config.ts',
+      components: ['tamagui'],
+    }),
+  ],
 })
